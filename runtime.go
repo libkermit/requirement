0a038e6c3130d9643beeab8a2d152e5d605caf1d
Vincent Demeester <vincent.demeester@docker.com>
2017-05-10 16:08:50 +0200

    Fix default runtime requirements

--- runtime.go
+++ runtime.go
@@ -3,16 +3,22 @@ package requirement
 import "runtime"
 
 // ArchitectureIs returns if the current architecture is the same as the specified one
-func ArchitectureIs(arch string) bool {
-	return runtime.GOARCH == arch
+func ArchitectureIs(arch string) func() bool {
+	return func() bool {
+		return runtime.GOARCH == arch
+	}
 }
 
 // OperatingSystemIs returns if the current operating system is the same as the specified one
-func OperatingSystemIs(os string) bool {
-	return runtime.GOOS == os
+func OperatingSystemIs(os string) func() bool {
+	return func() bool {
+		return runtime.GOOS == os
+	}
 }
 
 // GoVersionIs returns if the current go version is the same as the specified one
-func GoVersionIs(version string) bool {
-	return runtime.Version() == version
+func GoVersionIs(version string) func() bool {
+	return func() bool {
+		runtime.Version() == version
+	}
 }
